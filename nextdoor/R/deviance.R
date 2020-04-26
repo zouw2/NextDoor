5652c72f22d0c76c0f5f971db19ab7b498339a9d
Wei Zou - Regular <zouw2@gred-rstudio-p01.sc1.roche.com>
2020-04-25 17:51:02 -0700

    implement cox log likelihood

--- nextdoor/R/deviance.R
+++ nextdoor/R/deviance.R
@@ -27,9 +27,15 @@ deviances <- function(ypred, y, family, model0, weights=NULL){
     }else if (family == "poisson"){
         errors = 2*(y*log(ypred)-(y-ypred))
     }else{
-        stop("model familly not supported!")
+        if (family == 'cox'){
+            errors = logpl1(pred = ypred, surv.time = y[,1], surv.event=y[, 2]) 
+        }else {
+            stop( paste("model family", family, "not supported!") )
+        }
     }
-    if(is.null(weights) || family %in% 'cox'){ # deviance function for cox will handle weights
+    
+    
+    if(is.null(weights)  ){ 
      return(errors)
     }else{
         stopifnot(is.vector(weights))
@@ -39,4 +45,41 @@ deviances <- function(ypred, y, family, model0, weights=NULL){
         errors = errors * weights 
         
     }
+}
+
+
+logpl1 <- function(pred, surv.time, surv.event) {	
+    
+    # from https://github.com/bhklab/survcomp/blob/master/R/logpl.R
+    
+    n <- length(pred)
+    r <- rank(surv.time)
+    ita <- pred
+    epita <- exp(ita)
+    d <- rep(0, n)
+    dono <- rep(0, n)
+    # for(i in 1:n) {
+    #     d[i] <- sum(surv.event[r == r[i]])
+    #     dono[i] <- sum(epita[r >= r[i]])
+    # }
+    
+    dono <- sapply(r, function(r_i) sum(epita[r >= r_i]))
+    
+    if (F) { # wei disabled this: not relevant here.
+    risk <- d/dono
+    risk1 <- d/dono^{	2}
+    culrisk1 <- culrisk <- rep(0, n)
+    for(i in 1:n) {
+        culrisk[i] <- sum(unique(risk[r <= r[i]]))
+        culrisk1[i] <- sum(unique(risk1[r <= r[i]]))
+    }
+    
+
+    lik <- sum((ita - log(dono)) * surv.event)
+    res <- c(lik, sum(surv.event))
+    names(res) <- c("logpl", "event")
+    return(res)
+    }
+    
+    return( (ita - log(dono)) * surv.event * -2) # need the likelihood for indivual patients. multiply -2 so that the smaller the value the better
 }
